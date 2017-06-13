c704995c85b3b02c857fb93ac0f940462588c64f
jimrex <ijimrex@gmail.com>
2017-06-13 14:41:18 +0800

    bug

--- divide.py
+++ divide.py
@@ -5,8 +5,10 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 dictionary={"温度":"温度","机房":"机房","房间":"机房","室内":"机房","查询":"查询","看看":"查询","看一看":"查询","检查":"查询","湿度":"湿度","坏掉":"损坏"}
-st={"查询 温度 机房":"check the temperature","查询 湿度":"check the moisture"}
-sentence="机房"
+
+st={"查询 机房":"check the temperature","查询 湿度 机房":"check the moisture"}
+
+sentence="我要查询机房的湿度，但是也想看"
 
 def divide(str):
 	#return the unicode format result
@@ -24,9 +26,7 @@ def filt(li,type):
 	#get the specific words depending on the type you want
 	rli=[]
 	for w in li:
-
 		if w[1]==type:
-
 			rli.append(w[0])
 	return rli
 
@@ -52,7 +52,7 @@ def getPrefixHit(setType,store):
 		# print store
 		# print setStore&setType
 		count[store.keys()[i]]=len(setStore&setType)
-	print count
+	# print count
 	return count
 
 def ranking(count,setType):
@@ -60,17 +60,27 @@ def ranking(count,setType):
 	p={}
 	for x in count.keys():
 		p[x]=float(count[x]/float(N))
+	p=sort(p)
 	return p
 
+def sort(p):
+	dicts= sorted(p.iteritems(), key=lambda d:d[1], reverse = True)
+	return dicts
+	# print dicts
 
-# m=set([1,2,4])
+def excuteREST(p,st):
+
+	# print st[p[0][0]]
+	return st[p[0][0]]
 
 
 a=divide(sentence)
 b=getQueryTypeSet(a,dictionary)
 c=getPrefixHit(b,st)
 d=ranking(c,b)
+e=excuteREST(d,st)
+
 # b=filt(a,'v')
 
-print d
+print e
 
