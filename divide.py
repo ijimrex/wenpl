3b5d3be071ecbadb3ef8f27fd3d0171fd309b0e6
jimrex <ijimrex@gmail.com>
2017-06-21 15:38:11 +0800

    1 parameter

--- divide.py
+++ divide.py
@@ -5,7 +5,9 @@ import sys
 import urllib2  
 import json 
 import re 
+import copy
 jieba.load_userdict('../wendata/dict/dict.txt')
+jieba.load_userdict('../wendata/dict/dict_manual.txt')
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
@@ -85,7 +87,7 @@ def divide(str):
     li=[]
 
     for w in words:
-    	# print w.word
+    	print w.word
     	# print w.flag
     	li.append([w.word.encode('utf-8'),w.flag.encode('utf-8')])
     return li
@@ -99,24 +101,26 @@ def filt(li,type):
 	return rli
 
 def paraFilter(store):
+	#check parameters in store
+	dictionary={}
 	for x in store.keys():
+		dictionary[x]=[]
 		for y in x.split(" "):
-			t=re.match( r'\w', y)
-			if t!=None:
-				
-
-
-
-
-
-
-
+			j=[]
+			j=re.findall( r'\w+', y)
+			if j!=[]:
+				dictionary[x].append(j)
+	# print dictionary
+	return dictionary
 
 def getQueryTypeSet(li,dictionary,para,pro,paraCategory):
 	#calculate the types of the query words
 	# showp()
 	qType=[]
 	Nkey=0
+	hasPosition=0
+	hasName=0
+	paradic={}
 	# print dictionary
 	for w in li:
 		word=w[0]
@@ -125,10 +129,9 @@ def getQueryTypeSet(li,dictionary,para,pro,paraCategory):
 			if pro.has_key(word):
 				Nkey+=1
 			if paraCategory.has_key(word):
-				para.append(word)
-			# print dictionary[word]
-	# print qType
-	# print Nkey
+				paradic[paraCategory[word]]=word
+	for x in paradic.values():
+		para.append(x)
 	if Nkey==0:
 		return 0 		
 	setType=set(qType)
@@ -150,8 +153,6 @@ def getPrefixHit(setType,store):
 
 	# print count
 	return count
-
-
 
 def ranking(count,setType):
 	#calculate the probability
@@ -179,56 +180,77 @@ def revranking(count):
 	return p
 
 
-def excuteREST(p,rp,st,para):
-	# print p
+def excuteREST(p,rp,st,para,paraDict):
+	#p:正排序后的store匹配度列表
+	#rp:反排序后的store匹配度列表
+	#st:store字典 
+	#para:输入语句中的参数列表
+	#paraDict: store中参数列表
+	# print showList()
 	#p[[[],[]],[]]
 	#st{:}
-	turl='../wendata/token' 
-	fin1=open(turl,'r+')
-	token=fin1.read()
-	url=st[p[0][0]]
-	if len(para)!=0:
-		url+=para[0]
+	p=resort(p,rp)
+	print p[0][0]
+	if len(para)==0:
+		for x in p:
+			if len(paraDict[x[0]])==0:
+				url=st[x[0]]
+				break
+	elif len(para)==1:
+		for x in p:
+			if len(paraDict[x[0]])==1:
+				url=st[x[0]]+para[0]
+				break
+	else:
+		for x in p:
+			if len(paraDict[x[0]])==2:
+				url=st[x[0]][0]+para[0]+st[x[0]][1]+para[1]
+				break
+
+	# url=st[p[0][0]]
+	# if len(para)!=0:
+	# 	url+=para[0]
 	# print url
 	return getResult(url)
 
-def getDict(url):
-	try:
-		data = open(url,"r+").read()
-		# print data
-		return data
-	except Exception as e:
-		print e
-
-# def getResult():
-#     turl='../wendata/token'  
-#     qurl='../wendata/urls'
-#     fin1=open(turl,'r+')
-#     fin2=open(qurl,'r+')
-#     token=fin1.read()
-#     url=fin2.read()
-#     req=urllib2.Request(url)
-#     req.add_header('authorization',token)
-#     response = urllib2.urlopen(req)
-#     fin1.close()
-#     fin2.close()
-#     # print response.read()
-#     return response.read()
 
 def getResult(url):
-    print url
+    # print url
     turl='../wendata/token'  
     fin1=open(turl,'r+')
     token=fin1.read()
     url='http://www.intellense.com:3080'+url
-    print url
+    # print url
     req=urllib2.Request(url)
     req.add_header('authorization',token)
-    response = urllib2.urlopen(req)
+    # response = urllib2.urlopen(req)
     fin1.close()
     # print response.read()
     return url
     # return response.read()
+def resort(l1,l2):
+	# 反向检查匹配度
+	# print l2
+	l1=copy.deepcopy(l1)
+	l2=copy.deepcopy(l2)
+	for x in range(len(l1)-1):
+		if l1[x][1]==l1[x+1][1]:
+			for y in range(len(l2)-1):
+				if l2[y][0]==l1[x][0]:
+					break
+			for z in range(len(l2)):
+				if l2[z][0]==l1[x+1][0]:
+					break
+			# print y 
+			# print z
+			if y>z:
+				temp=copy.deepcopy(l1[x+1])
+				l1[x+1]=copy.deepcopy(l1[x])
+				l1[x]=copy.deepcopy(temp)
+	return l1
+
+
+
 
 def connectTuring(a):
 	kurl='../wendata/turkey'
@@ -240,7 +262,13 @@ def connectTuring(a):
 	fin.close()
 	# print reson['text'],'\n'
 	return reson['text']        
-
+def getDict(url):
+	try:
+		data = open(url,"r+").read()
+		# print data
+		return data
+	except Exception as e:
+		print e	
 def toUTF8(origin):
 	#change unicode type dict to UTF-8
 	result={}
@@ -255,8 +283,21 @@ def showDict(l):
 def showList(l):
 	for x in l:
 		print l
-
-sentence="查询 杭州市 报警"#todo：需要预处理一下，去掉空格和无意义符号
+# def getResult():
+#     turl='../wendata/token'  
+#     qurl='../wendata/urls'
+#     fin1=open(turl,'r+')
+#     fin2=open(qurl,'r+')
+#     token=fin1.read()
+#     url=fin2.read()
+#     req=urllib2.Request(url)
+#     req.add_header('authorization',token)
+#     response = urllib2.urlopen(req)
+#     fin1.close()
+#     fin2.close()
+#     # print response.read()
+#     return response.read()
+sentence="查看设备类型"#todo：需要预处理一下，去掉空格和无意义符号
 sentence=sentence.replace(' ', '')
 people=getPeople()
 cities=getPosition('cities')
@@ -272,11 +313,14 @@ dict2=dict(dict1, **paraCategory)
 st=getStore()#store dict
 para=[]
 keyphrase=pro.keys()
-paraFilter(st)
+# print st
+paraDict=paraFilter(st)
+# print 'dic' 
+# print paraDict
 # keyphrase.append(positions.keys())
 divideResult=divide(sentence)#list
 sentenceResult=getQueryTypeSet(divideResult,dict2,para,pro,paraCategory)#set
-
+# print para
 # print sentenceResult
 
 
@@ -289,7 +333,7 @@ else:
 	rerankingResult=revranking(hitResult)
 	# showList(rankResult)
 	# print rerankingResult
-	excuteResult=excuteREST(rankResult,rerankingResult,st,para)
+	excuteResult=excuteREST(rankResult,rerankingResult,st,para,paraDict)
 	# b=filt(a,'v')
 	print ""
 	print excuteResult
